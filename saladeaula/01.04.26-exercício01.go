05a0e68c578cfd43d8c60b0565e017bdfbbd907f
Gabriel Alves Martins <gabrielalves0@discente.ufg.br>
2026-04-01 19:34:11 -0300

    Implement addition function and main logic

--- saladeaula/01.04.26-exercício01.go
+++ saladeaula/01.04.26-exercício01.go
@@ -1,1 +1,16 @@
+package main
+
+import f "fmt"
+
+func soma(a, b float64) float64 {
+	r := a + b
+	return r
+}
+
+func main() {
+	var n1, n2 float64
+	f.Scan(&n1, &n2)
+	resultado := soma(n1, n2)
+	f.Println(resultado)
+}
 
