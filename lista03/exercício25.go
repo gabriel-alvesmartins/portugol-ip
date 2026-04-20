f3f9d933134fd994a3e70a447202afb019a81ba6
Gabriel Alves Martins <gabrielalves0@discente.ufg.br>
2026-04-20 19:25:38 -0300

    Remove comment about denominator in exercise25.go
    Removed comment explaining the denominator range.

--- lista03/exercício25.go
+++ lista03/exercício25.go
@@ -6,7 +6,6 @@ func main() {
 	numerador := 1.0
 	sinal := 1.0
 
-	// O denominador começa em 15^2 (225) e vai até 1^2 (1), logo são 15 termos.
 	for i := 15.0; i >= 1.0; i-- {
 		denominador := i * i
 		soma += sinal * (numerador / denominador)
