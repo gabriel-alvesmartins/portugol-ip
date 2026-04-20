e5b925b80cd2f540980626c3edc7757a6b9e5633
Gabriel Alves Martins <gabrielalves0@discente.ufg.br>
2026-04-20 19:24:07 -0300

    Add series sum calculation in exercício23.go
    Implement a program to calculate a series sum based on user input.

---
+++ lista03/exercício23.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("Digite N: ")
+	fmt.Scan(&n)
+
+	soma := 0.0
+	sinal := 1.0
+	numerador := 1000.0
+
+	for i := 1.0; i <= float64(n); i++ {
+		soma += sinal * (numerador / i)
+		numerador -= 3.0
+		sinal *= -1.0 // Alterna o sinal
+	}
+	fmt.Printf("O resultado é: %.2f\n", soma)
+}
