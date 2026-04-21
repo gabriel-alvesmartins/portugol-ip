2c5fe734959cb807ca333f275571edea9f01cdd4
Gabriel Alves Martins <gabrielalves0@discente.ufg.br>
2026-04-21 17:13:56 -0300

    Implement array inversion using recursion
    Refactor main function to use inverteArray for reversing the array.

--- lista04/exercício03.go
+++ lista04/exercício03.go
@@ -2,28 +2,25 @@ package main
 
 import "fmt"
 
+
+func inverteArray(arr []int, n int) {
+	
+	if n <= 1 {
+		return
+	}
+
+	arr[0], arr[n-1] = arr[n-1], arr[0]
+
+	inverteArray(arr[1:n-1], n-2)
+}
+
 func main() {
-	var n int
+	numeros := []int{10, 20, 30, 40, 50}
+	tamanho := len(numeros)
 
-	fmt.Println("Digite o valor de n")
-	fmt.Scan(&n)
+	fmt.Println("Array Original:", numeros)
 
-	v := make([]int, n)
+	inverteArray(numeros, tamanho)
 
-	for i := 0; i < n; i++ {
-		fmt.Scan(&v[i])
-	}
-
-	inicio := 0
-	fim := n - 1
-
-	for inicio < fim {
-		v[inicio], v[fim] = v[fim], v[inicio]
-		inicio++
-		fim--
-	}
-
-	for i := 0; i < n; i++ {
-		fmt.Print(v[i], " ")
-	}
+	fmt.Println("Array Invertido:", numeros)
 }
