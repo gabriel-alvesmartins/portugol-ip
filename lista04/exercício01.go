1d0983fea96971215f1a9b848c71dfb7123c6128
Gabriel Alves Martins <gabrielalves0@discente.ufg.br>
2026-04-21 16:31:45 -0300

    Implement power function and user input in Go

---
+++ lista04/exercício01.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func potencia(x, n int) int {
+	if n == 0 {
+		return 1
+	}
+	return x * potencia(x, n-1)
+}
+
+func main() {
+
+	var x, n int
+
+	fmt.Print("Digite x: ")
+	fmt.Scan(&x)
+
+	fmt.Print("Digite n: ")
+	fmt.Scan(&n)
+
+	resultado := potencia(x, n)
+
+	fmt.Printf("%d elevado a %d = %d\n", x, n, resultado)
+}
