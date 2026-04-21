fc8fbdd6bd6c66c5e921a7deee5f4208adf02687
Gabriel Alves Martins <gabrielalves0@discente.ufg.br>
2026-04-21 16:32:34 -0300

    Add array reversal program in exercício03.go
    Implement a program to reverse an array of integers.

---
+++ lista04/exercício03.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+
+	fmt.Println("Digite o valor de n")
+	fmt.Scan(&n)
+
+	v := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&v[i])
+	}
+
+	inicio := 0
+	fim := n - 1
+
+	for inicio < fim {
+		v[inicio], v[fim] = v[fim], v[inicio]
+		inicio++
+		fim--
+	}
+
+	for i := 0; i < n; i++ {
+		fmt.Print(v[i], " ")
+	}
+}
