072713f554cb1733b3aded37c7d57546bd970b6d
Gabriel Alves Martins <gabrielalves0@discente.ufg.br>
2026-04-21 16:32:53 -0300

    Create exercício04.go

---
+++ lista04/exercício04.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+
+	fmt.Println("Digite o valor de n")
+	fmt.Scan(&n)
+
+	if n == 0 {
+		fmt.Println(0)
+		return
+	}
+
+	bin := []int{}
+
+	for n > 0 {
+		resto := n % 2
+		bin = append(bin, resto)
+		n = n / 2
+	}
+
+	for i := len(bin) - 1; i >= 0; i-- {
+		fmt.Print(bin[i])
+	}
+}
